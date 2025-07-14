c71efc00c07bac5bb941ac80839685cdb83b966d
“lfrclnando” <lfernando.div@gmail.com>
2025-07-14 03:15:58 -0300

    Feat: many features

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+// biome-ignore assist/source/organizeImports: <can be yet>
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
